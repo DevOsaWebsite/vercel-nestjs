416fb1afe39bc8739b86583db7833b05ffad34b2
DevOsa <osadchenko.serghey@icloud.com>
2024-01-19 20:23:48 +0200

    IS_PRODUCTION

--- src/app.service.ts
+++ src/app.service.ts
@@ -6,7 +6,8 @@ export class AppService {
   constructor(private configService: ConfigService) {}
   getHello(): string {
     const message = this.configService.get<string>('MESSAGE');
-    const IS_PRODUCTION = this.configService.get<string>('NODE_ENV') === 'production';
+    const IS_PRODUCTION =
+      this.configService.get<string>('NODE_ENV') === 'production';
 
     return message + ' ' + IS_PRODUCTION;
   }
