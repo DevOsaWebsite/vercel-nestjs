542090c62c8cf0e743bf4c7b0af40cc4ce7744e2
DevOsa <osadchenko.serghey@icloud.com>
2024-01-19 21:00:03 +0200

    add async

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,13 +4,20 @@ import { ConfigService } from '@nestjs/config';
 @Injectable()
 export class AppService {
   private readonly logger = new Logger(AppService.name);
-  constructor(private configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) {}
   async getHello(): Promise<string> {
     const message = this.configService.get<string>('MESSAGE');
     const NODE_ENV = this.configService.get<string>('NODE_ENV');
     const IS_PRODUCTION = this.configService.get<string>('IS_PRODUCTION');
 
-    const result = message + ' ' + `${IS_PRODUCTION}` + ' ' + `${NODE_ENV}`;
+    const result =
+      message +
+      ' ' +
+      `${IS_PRODUCTION}` +
+      ' ' +
+      `${NODE_ENV}` +
+      ' ' +
+      'from AppService';
 
     this.logger.log(`Message: ${result}`);
     return result;
