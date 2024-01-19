c1521ee23e28824978b8ec0c06ad59989d16f9aa
DevOsa <osadchenko.serghey@icloud.com>
2024-01-19 21:01:42 +0200

    clean service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,25 +1,25 @@
-import { Injectable, Logger } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+// import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  private readonly logger = new Logger(AppService.name);
-  constructor(private readonly configService: ConfigService) {}
+  // private readonly logger = new Logger(AppService.name);
+  // constructor(private readonly configService: ConfigService) {}
   async getHello(): Promise<string> {
-    const message = this.configService.get<string>('MESSAGE');
-    const NODE_ENV = this.configService.get<string>('NODE_ENV');
-    const IS_PRODUCTION = this.configService.get<string>('IS_PRODUCTION');
+    // const message = this.configService.get<string>('MESSAGE');
+    // const NODE_ENV = this.configService.get<string>('NODE_ENV');
+    // const IS_PRODUCTION = this.configService.get<string>('IS_PRODUCTION');
 
-    const result =
-      message +
-      ' ' +
-      `${IS_PRODUCTION}` +
-      ' ' +
-      `${NODE_ENV}` +
-      ' ' +
-      'from AppService';
+    // const result =
+    //   message +
+    //   ' ' +
+    //   `${IS_PRODUCTION}` +
+    //   ' ' +
+    //   `${NODE_ENV}` +
+    //   ' ' +
+    //   'from AppService';
 
-    this.logger.log(`Message: ${result}`);
+    // this.logger.log(`Message: ${result}`);
     return 'result';
   }
 }
