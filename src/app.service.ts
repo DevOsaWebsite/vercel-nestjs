e3e8521af985279803988fecb9eefa56b4f56579
DevOsa <osadchenko.serghey@icloud.com>
2024-01-19 20:38:04 +0200

    test logger

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
   constructor(private configService: ConfigService) {}
   getHello(): string {
     const message = this.configService.get<string>('MESSAGE');
-    const IS_PRODUCTION = this.configService.get<string>('IS_PROD');
+    const NODE_ENV = this.configService.get<string>('NODE_ENV');
+    const IS_PRODUCTION = this.configService.get<string>('IS_PRODUCTION');
 
-    return message + ' ' + `${IS_PRODUCTION}`;
+    const result = message + ' ' + `${IS_PRODUCTION}` + ' ' + `${NODE_ENV}`;
+
+    this.logger.log(`Message: ${result}`);
+    return result;
   }
 }
