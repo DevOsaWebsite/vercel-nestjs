1664beebd8b40fa66e781b9eb658f692cd93cec6
DevOsa <osadchenko.serghey@icloud.com>
2024-01-19 21:07:35 +0200

    rollback

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,25 +1,8 @@
 import { Injectable } from '@nestjs/common';
-// import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-  // private readonly logger = new Logger(AppService.name);
-  // constructor(private readonly configService: ConfigService) {}
   getHello() {
-    // const message = this.configService.get<string>('MESSAGE');
-    // const NODE_ENV = this.configService.get<string>('NODE_ENV');
-    // const IS_PRODUCTION = this.configService.get<string>('IS_PRODUCTION');
-
-    // const result =
-    //   message +
-    //   ' ' +
-    //   `${IS_PRODUCTION}` +
-    //   ' ' +
-    //   `${NODE_ENV}` +
-    //   ' ' +
-    //   'from AppService';
-
-    // this.logger.log(`Message: ${result}`);
     return 'result';
   }
 }
